215504250b2c27fa8b159bd5cac4ea6560b5bf7a
MatsunamiDaiki <lax.md0@gmail.com>
2024-03-20 21:19:53 +0900

    reacttouter追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import './App.css'
+import Main from './components/Main'
+import NotFound from './components/NotFound'
 import SignIn from './components/SignIn'
+import SignUp from './components/SignUp'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
     <div className='App'>
-      <SignIn/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/signin' element={<SignIn />}/>
+          <Route path='/signup' element={<SignUp />}/>
+          <Route path='/' element={<Main />}/>
+          <Route path='*' element={<NotFound />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
