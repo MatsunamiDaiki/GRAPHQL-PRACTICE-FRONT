9a61c683dd2b587b86f59d5328d3c3a9b1c13b02
MatsunamiDaiki <lax.md0@gmail.com>
2024-03-20 21:46:17 +0900

    認証によるルーティングの出しわけ

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,18 @@ import NotFound from './components/NotFound'
 import SignIn from './components/SignIn'
 import SignUp from './components/SignUp'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { GuestRoute, PrivateRoute } from './AuthRoute'
 
 function App() {
-
   return (
-    <div className='App'>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/signin' element={<SignIn />}/>
-          <Route path='/signup' element={<SignUp />}/>
-          <Route path='/' element={<Main />}/>
-          <Route path='*' element={<NotFound />}/>
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/signin' element={<GuestRoute children={<SignIn />} />} />
+        <Route path='/signup' element={<GuestRoute children={<SignUp />} />}/>
+        <Route path='/' element={<PrivateRoute children={<Main />} />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
